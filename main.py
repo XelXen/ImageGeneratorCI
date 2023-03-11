418c6c972747f50982525bdd3fe991d2ed93ee01
XelXen <78258352+XelXen@users.noreply.github.com>
2023-03-11 15:43:00 +0530

    Update Code to inherit prompts externlly

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ from json import loads
 model_id = "dreamlike-art/dreamlike-photoreal-2.0"
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float32)
 
-prompt = "photo, a church in the middle of a field of crops, bright cinematic lighting, gopro, fisheye lens"
-image = pipe(prompt).images[0]
+with open('prompt.txt', 'r') as file:
+    prompt = file.read()
+    print(f"Prompt: {prompt.strip()}")
 
+image = pipe(prompt).images[0]
 image.save("result.jpg")
 
 status = loads(requests.get("https://api.gofile.io/getServer").text)
 upload = loads(requests.post(f"https://{status['data']['server']}.gofile.io/uploadFile", files = {'file': open("result.jpg" ,'rb')}).text)['data']['downloadPage']
 
-print(upload)
+print(f"\nDownload Link: {upload}")
