33ff470cdbbc66f9a957773e9b7213934b572466
XelXen <78258352+XelXen@users.noreply.github.com>
2023-03-11 14:10:39 +0530

    Add upload function

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from diffusers import StableDiffusionPipeline
 import torch
+import requests
+from json import loads
 
 model_id = "dreamlike-art/dreamlike-photoreal-2.0"
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float32)
@@ -8,3 +10,8 @@ prompt = "photo, a church in the middle of a field of crops, bright cinematic li
 image = pipe(prompt).images[0]
 
 image.save("result.jpg")
+
+status = loads(requests.get("https://api.gofile.io/getServer").text)
+upload = loads(requests.post(f"https://{status['data']['server']}.gofile.io/uploadFile", files = {'file': open("result.jpg" ,'rb')}).text)['data']['downloadPage']
+
+print(upload)
