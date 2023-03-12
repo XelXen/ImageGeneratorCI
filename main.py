59c96edd196f804641e8ec31860925c0901a58c4
XelXen <78258352+XelXen@users.noreply.github.com>
2023-03-12 22:06:55 +0530

    Add device checker

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from json import loads
 
 model_id = "dreamlike-art/dreamlike-photoreal-2.0"
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float32)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+pipe = pipe.to(device)
 
 with open('prompt.txt', 'r') as file:
     prompt = file.read()
